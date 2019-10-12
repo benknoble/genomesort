e6e710f4359dac50c643bd4f5d2f3b8c5cfaa681
D. Ben Knoble <ben.knoble+github@gmail.com>
2019-10-12 16:20:53 -0400

    game: make a movable gnome!

--- js/game.js
+++ js/game.js
@@ -17,11 +17,31 @@ var config = {
 
 new Phaser.Game(config);
 
+var player;
+var cursors;
+
 function preload() {
+  this.load.image('gnome', 'assets/gnome.png');
 }
 
 function create() {
+  player = this.physics.add.image(100, 450, 'gnome');
+  player.setCollideWorldBounds(true);
+  cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
+  player_move()
+}
+
+function player_move() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+  }
+  else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+  }
+  else {
+    player.setVelocityX(0);
+  }
 }
