06add19c31b8a7f21b3b4a46ccf6e166af006d69
D. Ben Knoble <ben.knoble+github@gmail.com>
2019-10-12 16:01:39 -0400

    game: pull out game

---
+++ js/game.js
@@ -0,0 +1,27 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+new Phaser.Game(config);
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
